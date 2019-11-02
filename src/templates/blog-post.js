fe2cc3b99968a5fb4095089d975439030956cd18
Shaun <shaungithuku@gmail.com>
2019-11-02 12:53:38 +0000

    Added Tina CMS

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,50 +1,51 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { Link, graphql } from "gatsby";
-import get from "lodash/get";
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import { remarkForm } from 'gatsby-tinacms-remark'
 
-import Bio from "../components/Bio";
-import Layout from "../components/layout";
-import { rhythm, scale } from "../utils/typography";
-import styled from "styled-components";
+import Bio from '../components/Bio'
+import Layout from '../components/layout'
+import { rhythm, scale } from '../utils/typography'
+import styled from 'styled-components'
 
 const Header = styled.h2`
   padding: 10px 10px;
   text-align: center;
   font-size: 3rem;
   line-height: 3rem;
-`;
+`
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark;
-    const siteTitle = get(this.props, "data.site.siteMetadata.title");
-    const siteDescription = post.excerpt;
-    const { previous, next } = this.props.pageContext;
+    const post = this.props.data.markdownRemark
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const siteDescription = post.excerpt
+    const { previous, next } = this.props.pageContext
 
     return (
       <Layout location={this.props.location}>
         <Helmet
-          htmlAttributes={{ lang: "en" }}
-          meta={[{ name: "description", content: siteDescription }]}
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
           title={`${post.frontmatter.title} | ${siteTitle}`}
         />
         <Header>{post.frontmatter.title}</Header>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <br
           style={{
-            marginBottom: rhythm(3)
+            marginBottom: rhythm(3),
           }}
         />
         <Bio />
 
         <ul
           style={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-between",
-            listStyle: "none",
-            padding: 0
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            listStyle: 'none',
+            padding: 0,
           }}
         >
           {previous && (
@@ -64,11 +65,11 @@ class BlogPostTemplate extends React.Component {
           )}
         </ul>
       </Layout>
-    );
+    )
   }
 }
 
-export default BlogPostTemplate;
+export default remarkForm(BlogPostTemplate)
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -86,6 +87,9 @@ export const pageQuery = graphql`
         title
         date(formatString: "MMMM DD, YYYY")
       }
+      fileRelativePath
+      rawFrontmatter
+      rawMarkdownBody
     }
   }
-`;
+`
